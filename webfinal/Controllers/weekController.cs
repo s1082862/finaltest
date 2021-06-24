ced40f16ed313260838f68df77b91ce1717da9e5
s1082862 <ylec0217@gmail.com>
2021-06-24 19:35:46 +0800

    Q4

---
+++ webfinal/Controllers/weekController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace webfinal.Controllers
+{
+    public class weekController : Controller
+    {
+        // GET: week
+        public ActionResult Index(float? num)
+        {
+            var result = "";
+            if (num.HasValue)
+            {
+                result = "星期"+num;
+            }
+            ViewBag.Name = num;
+            ViewBag.Result = result;
+            return View();
+        }
+    }
+}
